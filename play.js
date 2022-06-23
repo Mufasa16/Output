1e136cf2f14ad49038f96ecacf5da251948227e4
Mufasa <officialtheo16@gmail.com>
2022-06-23 13:17:37 +0100

    first commit

---
+++ play.js
@@ -0,0 +1,8 @@
+var first_name = "Theophilus";
+var last_name = "Okonkwo"; 
+var height= '176cm';
+var country= "Nigeria";
+console.log(first_name);
+console.log(last_name);
+console.log(height)
+console.log(country)
